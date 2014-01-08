36f80567d9eda6ccca76a9ea90ba921f3bae60c3
Laurent Perrin <laurent.perrin@gmail.com>
2014-01-08 13:29:34 +0100

    sql creation script.

--- sql/twont.sql
+++ sql/twont.sql
@@ -1,15 +1,3 @@
-DROP TABLE IF EXISTS `tweets`;
-
-CREATE TABLE `tweets` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `text` VARCHAR(255) NOT NULL,
-  `user_screen_name` VARCHAR(128) NOT NULL,
-  `user_name` VARCHAR(255) NOT NULL,
-  `created_at` INT NOT NULL,
-  `nb_comments` INT NOT NULL,
-  PRIMARY KEY (`id`)
-);
-
 DROP TABLE IF EXISTS `comments`;
 
 CREATE TABLE `comments` (
@@ -17,10 +5,22 @@ CREATE TABLE `comments` (
   `uid` VARCHAR(10) NOT NULL,
   `body` VARCHAR(255) NOT NULL,
   `tweet_id` INT NOT NULL,
-  `date` INT NOT NULL,
+  `date` DATETIME NOT NULL,
   `author_name` VARCHAR(255),
   PRIMARY KEY (`id`),
   UNIQUE KEY(`uid`)
+);
+
+DROP TABLE IF EXISTS `tweets`;
+
+CREATE TABLE `tweets` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `text` VARCHAR(255) NOT NULL,
+  `user_screen_name` VARCHAR(128) NOT NULL,
+  `user_name` VARCHAR(255) NOT NULL,
+  `created_at` DATETIME NOT NULL,
+  `nb_comments` INT NOT NULL,
+  PRIMARY KEY (`id`)
 );
 
 ALTER TABLE `comments` ADD FOREIGN KEY (tweet_id) REFERENCES `tweets` (`id`) ON DELETE CASCADE;
